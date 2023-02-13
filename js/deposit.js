63aa7b7230b11b747d4243984699d5aa25a28b08
protapsaha1 <webprotap09@gmail.com>
2023-02-13 14:20:58 +0600

    code push

--- js/deposit.js
+++ js/deposit.js
@@ -1,4 +1,48 @@
 document.getElementById('deposit-btn').addEventListener('click', function(){
     const inputDeposit = document.getElementById('input-deposit');
-    const deposit = inputDeposit.value;
-})
+    const newDepositString = inputDeposit.value;
+    const newDeposit = parseFloat(newDepositString);
+
+    // current total deposit
+    const totalDeposit = document.getElementById('total-deposit');   
+    const previousDepositMoneyString = totalDeposit.innerText ;
+    const previousDepositMoney = parseFloat(previousDepositMoneyString);
+
+    // previousDepositMoney = newDeposit
+    const currentDepositTotal = previousDepositMoney + newDeposit;
+    totalDeposit.innerText = currentDepositTotal;
+    // add total deposit money in balance
+    const previousBalance = document.getElementById('balance');
+    const previousBalanceString = previousBalance.innerText;
+    const previousBalanceMoney = parseFloat(previousBalanceString);
+    const currentBalance = previousBalanceMoney + newDeposit; 
+    previousBalance.innerText = currentBalance ;
+    // it make clear input place after  
+
+    inputDeposit.value = '';
+});
+
+// withdraw money and total balance after withdraw
+
+
+document.getElementById('withdraw-btn').addEventListener('click', function(){
+    const inputWithdraw = document.getElementById('input-withdraw');
+    const inputWithdrawString = inputWithdraw.value;   
+    const newWithdraw = parseFloat(inputWithdrawString);
+    
+    const totalWithdraw = document.getElementById('total-withdraw');
+    const previousWithdrawString = totalWithdraw.innerText;
+    const previousWithdraw = parseFloat(previousWithdrawString);
+    
+    const currentWithdrawTotal = previousWithdraw + newWithdraw ;
+    totalWithdraw.innerText = currentWithdrawTotal;
+
+    const withdrawBalance = document.getElementById('balance');
+    const withdrawBalanceString = withdrawBalance.innerText;
+    const currentWithdrawBalance = parseFloat(withdrawBalanceString);
+
+    const currentBalance = currentWithdrawBalance - newWithdraw ;
+    withdrawBalance.innerText = currentBalance ;
+
+    inputWithdraw.value ='';
+});
