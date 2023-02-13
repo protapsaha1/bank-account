6c47fb7da9936f0702548f267afdc6b1a0e69a84
protapsaha1 <webprotap09@gmail.com>
2023-02-13 14:33:05 +0600

    code push

--- js/index.js
+++ js/index.js
@@ -4,12 +4,13 @@ document.getElementById('log-btn').addEventListener('click', function(){
     const userPass = document.getElementById('input-pass');
     const pass = userPass.value;
     if(email === 'webprotap@gmail.com' && pass === '123protap@@'){
-        window.location.href='bank.html';
+        window.location.href='bank.html'
     }
     else{
         alert('your email or password is not valid')
     }
 });
+
 // const user = document.getElementById('input-email', 'input-pass').addEventListener('keyup',function(event){
 //     const input = event.target.Value;
 //     const logInBtn = document.getElementById('log-btn');
