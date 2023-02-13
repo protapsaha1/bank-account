a8bc321986ea8f14b7b2e992962dff8ddf43935d
protapsaha1 <webprotap09@gmail.com>
2023-02-13 07:33:28 +0600

    code push

---
+++ js/deposit.js
@@ -0,0 +1,4 @@
+document.getElementById('deposit-btn').addEventListener('click', function(){
+    const inputDeposit = document.getElementById('input-deposit');
+    const deposit = inputDeposit.value;
+})
--- js/index.js
+++ js/index.js
@@ -1,13 +1,13 @@
 document.getElementById('log-btn').addEventListener('click', function(){
     const userEmail = document.getElementById('input-email');
-    const email = userEmail.Value;
+    const email = userEmail.value;
     const userPass = document.getElementById('input-pass');
-    const pass = userPass.Value;
+    const pass = userPass.value;
     if(email === 'webprotap@gmail.com' && pass === '123protap@@'){
-        console.log('valid user')
+        window.location.href='bank.html';
     }
     else{
-        console.log('invalid user')
+        alert('your email or password is not valid')
     }
 });
 // const user = document.getElementById('input-email', 'input-pass').addEventListener('keyup',function(event){
